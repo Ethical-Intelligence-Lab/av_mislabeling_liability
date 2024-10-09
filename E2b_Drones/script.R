6358fc8aff75504dbd0900427e2831f3fc4dda9f
justinhjy1004 <ethicallab@github.com>
2024-10-09 13:42:07 -0400

    drones

--- E2b_Drones/script.R
+++ E2b_Drones/script.R
@@ -5,7 +5,7 @@ rm(list = ls())
 library(dplyr)
 library(grid)
 if (!require(pacman)) {install.packages("pacman")}
-pacman::p_load('ggplot2',         # plotting
+pacman::p_load('tidyverse',       
                'ggsignif',        # plotting significance bars
                'lme4',            # functions for fitting linear regression models
                'ggforce',         # make ggplot even fancier
@@ -21,14 +21,14 @@ pacman::p_load('ggplot2',         # plotting
                'compute.es',      # effect size package
                'effsize',         # another effect size package
                'pwr',             # package for power calculation
-               'nlme',            # get p values for mixed effect model
+               'Hmisc',            # get p values for mixed effect model
                'DescTools',        # get Cramer's V
                'rstatix',
                'effects'
 )
 
 # PROCESS Analysis (Set TRUE if you wish to run PROCESS code)
-mediation <- F
+mediation <- T
 if(mediation) {
   source("../process.R")
 }
@@ -38,83 +38,88 @@ if(mediation) {
 ## ================================================================================================================
 
 ## read in data
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-d <- read.csv('data.csv') 
+d <- read_csv('data.csv')
 d <- d[-c(1,2),]
 
 d |>
-  filter(att_1 == 2, att_2 == 5) -> d
+  mutate_if(all.is.numeric, as.numeric) -> d
+
+## Attention Check
+d |>
+  filter(att_1 == 2 & att_2 == 5) -> d
+
+## Comp Check
+d |>
+  filter(comp_1 == 1 & comp_2 == 1) -> d
 
 nrow(d)
 
-d |>
-  filter(comp_1 == 2, comp_2 == 4) -> d
-
-nrow(d)
-
-d |>
-  mutate(
-    label = Tax.Auto.Prepare.Tax.Co.Prepare,
-    age = as.numeric(age),
-    automation_1 = as.numeric(automation_1),
-    software_resp_1 = as.numeric(software_resp_1),
-    software_liable_1 = as.numeric(software_liable_1),
-    human_resp_1 = as.numeric(human_resp_1),
-    human_liable_1 = as.numeric(human_liable_1)
-  ) -> d
-
 ## ================================================================================================================
-##                                            DEMOGRAPHICS                
+##                                                 DEMOGRAPHICS                 
 ## ================================================================================================================
 
-mean(d$age, na.rm = T)
+prop.table(table(d[d$gender == 1 | d$gender == 2,]$gender))[[1]]
+mean(d$age)
 
-prop.table(table(d$gender))[[1]]
+table(d$label)
 
 ## ================================================================================================================
-##                                            ANALYSES                
+##                                                 ANALYSIS                
 ## ================================================================================================================
 
-cronbach.alpha(d[,c("software_resp_1", "software_liable_1")])
-cronbach.alpha(d[,c("human_resp_1", "human_liable_1")])
+t.test(d[d$label == "Autopilot",]$automation_1,
+       d[d$label == "Copilot",]$automation_1)
 
-d |>
-  mutate(
-    firm_liability = (software_resp_1 + software_liable_1) / 2,
-    human_liability = (human_resp_1 + human_liable_1) / 2,
-  ) -> d
+sd(d[d$label == "Autopilot",]$automation_1)
+sd(d[d$label == "Copilot",]$automation_1)
 
-## Capability
-t.test(d[d$label == "Tax Auto-Prepare",]$automation_1, 
-       d[d$label == "Tax Co-Prepare",]$automation_1)
+cohen.d(d[d$label == "Autopilot",]$automation_1,
+        d[d$label == "Copilot",]$automation_1)
 
-sd(d[d$label == "Tax Auto-Prepare",]$automation_1)
-sd(d[d$label == "Tax Co-Prepare",]$automation_1)
+cronbach.alpha(d[,c("software_liable_1", "software_resp_1")])
+cronbach.alpha(d[,c("human_liable_1", "human_resp_1" )])
 
-cohen.d(d[d$label == "Tax Auto-Prepare",]$automation_1, 
-       d[d$label == "Tax Co-Prepare",]$automation_1)
+d$human <- (d$human_liable_1 + d$human_resp_1) / 2
+d$firm <-(d$software_liable_1 + d$software_resp_1) / 2
 
-## Firm Liability
-t.test(d[d$label == "Tax Auto-Prepare",]$firm_liability, 
-       d[d$label == "Tax Co-Prepare",]$firm_liability)
+### Firm Liability
 
-sd(d[d$label == "Tax Auto-Prepare",]$firm_liability)
-sd(d[d$label == "Tax Co-Prepare",]$firm_liability)
+t.test(d[d$label == "Autopilot",]$firm,
+       d[d$label == "Copilot",]$firm)
 
-cohen.d(d[d$label == "Tax Auto-Prepare",]$firm_liability, 
-        d[d$label == "Tax Co-Prepare",]$firm_liability)
+sd(d[d$label == "Autopilot",]$firm)
+sd(d[d$label == "Copilot",]$firm)
 
-## Human Liability
-t.test(d[d$label == "Tax Auto-Prepare",]$human_liability, 
-       d[d$label == "Tax Co-Prepare",]$human_liability)
+cohen.d(d[d$label == "Autopilot",]$firm,
+        d[d$label == "Copilot",]$firm)
 
-sd(d[d$label == "Tax Auto-Prepare",]$human_liability)
-sd(d[d$label == "Tax Co-Prepare",]$human_liability)
+### Human Liability 
 
-cohen.d(d[d$label == "Tax Auto-Prepare",]$human_liability, 
-        d[d$label == "Tax Co-Prepare",]$human_liability)
+t.test(d[d$label == "Autopilot",]$human,
+       d[d$label == "Copilot",]$human)
 
+sd(d[d$label == "Autopilot",]$human)
+sd(d[d$label == "Copilot",]$human)
+
+cohen.d(d[d$label == "Autopilot",]$human,
+        d[d$label == "Copilot",]$human)
+
+## ================================================================================================================
+##                                              PROCESS             
+## ================================================================================================================
+
+
+d$cond <- as.numeric(as.factor(d$label))
+
+if(mediation) {
+  process(data = d, y = "firm", x = "cond", 
+          m =c("automation_1"), model = 4, effsize = 1, total = 1, stand = 1, 
+          contrast =1, boot = 10000 , modelbt = 1, seed = 654321)
+  
+  process(data = d, y = "human", x = "cond", 
+          m =c("automation_1"), model = 4, effsize = 1, total = 1, stand = 1, 
+          contrast =1, boot = 10000 , modelbt = 1, seed = 654321)
+}
 
 ## ================================================================================================================
 ##                                              DATA VIZUALIZATION              
@@ -122,11 +127,11 @@ cohen.d(d[d$label == "Tax Auto-Prepare",]$human_liability,
 
 # Renaming and labeling for plots
 d |>
-  select(label, automation_1, firm_liability, human_liability) |>
+  select(label, automation_1, firm, human) |>
   mutate(
-    `Marketing Label` = ifelse(label == "Tax Auto-Prepare", "Tax Auto-Prepare", "Tax Co-Prepare"),
-    Firm = firm_liability,
-    Human = human_liability,
+    `Marketing Label` = ifelse(label == "Autopilot", "Autopilot", "Copilot"),
+    Firm = firm,
+    Human = human,
     Capability = automation_1
   ) |>
   select(`Marketing Label`, Firm, Human, Capability) -> d_plot
@@ -134,7 +139,7 @@ d |>
 # Obtain mean and standard errors for condition and measure
 d_plot |>
   group_by(`Marketing Label`) |>
-  summarize(
+  summarise(
     avg_F = mean(Firm),
     avg_H = mean(Human),
     se_F = sd(Firm)/sqrt(n()),
@@ -152,8 +157,8 @@ ggplot(data = d_plot, aes(x=factor(`Marketing Label`), y=avg_F)) +
   geom_errorbar(aes(ymin=avg_F-(se_F*se_width), ymax=avg_F+(se_F*se_width)), position = "dodge", 
                 size=.25, color="black", width=.75) +
   geom_signif(
-    y_position = c(100), xmin = c("Tax Auto-Prepare"), xmax = c("Tax Co-Prepare"),
-    annotation = c("ns"), tip_length = 0.1, color='black', size = .5, textsize = 3.5
+    y_position = c(75), xmin = c("Autopilot"), xmax = c("Copilot"),
+    annotation = c("***"), tip_length = 0.1, color='black', size = .5, textsize = 3.5
   ) + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"),
@@ -173,8 +178,8 @@ ggplot(data = d_plot, aes(x=factor(`Marketing Label`), y=avg_H)) +
   geom_errorbar(aes(ymin=avg_H-(se_H*se_width), ymax=avg_H+(se_H*se_width)), position = "dodge", 
                 size=.25, color="black", width=.75) +
   geom_signif(
-    y_position = c(70), xmin = c("Tax Auto-Prepare"), xmax = c("Tax Co-Prepare"),
-    annotation = c("ns"), tip_length = 0.1, color='black', size = .5, textsize = 3.5
+    y_position = c(90), xmin = c("Autopilot"), xmax = c("Copilot"),
+    annotation = c("***"), tip_length = 0.1, color='black', size = .5, textsize = 3.5
   ) + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"),
@@ -192,7 +197,7 @@ p2
 ggarrange(p1,p2) |>
   annotate_figure(bottom = textGrob("Marketing Label", gp = gpar(cex = 1, fontsize=10, fontface="bold")))
 
-ggsave("liability_ascriptions.jpg", device = "jpg",width = 5.3, height = 3.7, units = "in")
+ggsave("liability.jpg", device = "jpg",width = 5.3, height = 3.7, units = "in")
 
 # Plot Level of Automation
 ggplot(data = d_plot, aes(x=factor(`Marketing Label`), y=avg_C)) +
@@ -201,7 +206,7 @@ ggplot(data = d_plot, aes(x=factor(`Marketing Label`), y=avg_C)) +
   geom_errorbar(aes(ymin=avg_C-(se_C*se_width), ymax=avg_C+(se_C*se_width)), position = "dodge", 
                 size=.25, color="black", width=.75) +
   geom_signif(
-    y_position = c(100), xmin = c("Tax Auto-Prepare"), xmax = c("Tax Co-Prepare"),
+    y_position = c(90), xmin = c("Autopilot"), xmax = c("Copilot"),
     annotation = c("***"), tip_length = 0.1, color='black', size = .5, textsize = 3.5
   ) + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
